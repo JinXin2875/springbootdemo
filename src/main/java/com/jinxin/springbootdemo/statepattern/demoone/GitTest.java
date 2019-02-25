f589ad4af73d7bcdde4f9f34a8d8aab6d7f53799
jinxin1 <jinxin1@guahao.com>
2019-02-25 15:30:26 +0800

    test

---
+++ src/main/java/com/jinxin/springbootdemo/statepattern/demoone/GitTest.java
@@ -0,0 +1,11 @@
+package com.jinxin.springbootdemo.statepattern.demoone;
+
+/**
+ * @author JinXin
+ * @Date 2019/2/25 15:12
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        
+    }
+}
