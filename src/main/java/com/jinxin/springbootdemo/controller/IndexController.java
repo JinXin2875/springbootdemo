937f54ce4b3be27e7ffc8fce4d58a266e80b90ae
jinxin1 <jinxin1@guahao.com>
2019-03-14 15:19:02 +0800

    mysql spring boot

--- src/main/java/com/jinxin/springbootdemo/controller/IndexController.java
+++ src/main/java/com/jinxin/springbootdemo/controller/IndexController.java
@@ -3,14 +3,17 @@ package com.jinxin.springbootdemo.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author JinXin
@@ -19,11 +22,22 @@ import java.util.List;
 @Controller
 @RequestMapping("index")
 public class IndexController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-
+    @ResponseBody
     @RequestMapping("/html/test")
     public String test(@RequestBody String index) {
        return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("/mysqlTest")
+    public String mysqlTest() {
+        String sql = "select * from user";
+        List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql);
+        System.out.println("123");
+        return "";
     }
 
 }
